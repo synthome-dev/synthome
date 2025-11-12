89b7787a64c5b7dc0d1a69a3dbc08f868bbeec59
Dmitry Dubovetzky <dubovetzky@me.com>
2025-11-12 12:03:13 +0100

    Added  SYNTHOME_API_KEY env var as a requirement

--- packages/ai-video-sdk/src/index.ts
+++ packages/ai-video-sdk/src/index.ts
@@ -2,102 +2,104 @@ export { replicate } from "./providers/replicate.js";
 export type {
   ReplicateModelId,
   ReplicateModels,
-  Seedance1ProOptions,
+  Seedance1ProOptions
 } from "./providers/replicate.js";
 
 export { fal } from "./providers/fal.js";
 export type {
-  FalModelId,
-  FalModels,
   Fabric1FastOptions,
+  FalModelId,
+  FalModels
 } from "./providers/fal.js";
 
 export { googleCloud } from "./providers/google-cloud.js";
 export type {
   GoogleCloudModelId,
-  GoogleCloudModels,
+  GoogleCloudModels
 } from "./providers/google-cloud.js";
 
 export { generateVideo as generateVideoAPI } from "./generators/generate-video-api.js";
 export type { GenerateVideoOptions as GenerateVideoAPIOptions } from "./generators/generate-video-api.js";
 
 export type {
+  AudioModel,
   BaseGenerateOptions,
+  ImageModel,
   JobStatus,
   PollingConfig,
   ProviderConfig,
   VideoJob,
   VideoModel,
-  ImageModel,
-  AudioModel,
   VideoProvider,
-  WebhookConfig,
+  WebhookConfig
 } from "./core/types.js";
 
 export { compose } from "./compose/pipeline.js";
 export type {
-  Pipeline,
-  PipelineProgress,
-  PipelineExecution,
   ExecuteOptions,
+  Pipeline,
+  PipelineExecution,
+  PipelineProgress
 } from "./compose/pipeline.js";
 
 export { generateVideo } from "./compose/generate-video.js";
 export type {
   GenerateVideoOptions,
-  GenerateVideoUnified,
   GenerateVideoProvider,
+  GenerateVideoUnified
 } from "./compose/generate-video.js";
 
 export { generateImage } from "./compose/generate-image.js";
 export type {
   GenerateImageOptions,
-  GenerateImageProvider,
+  GenerateImageProvider
 } from "./compose/generate-image.js";
 
 export { generateAudio } from "./compose/generate-audio.js";
 export type {
   GenerateAudioOptions,
-  GenerateAudioProvider,
+  GenerateAudioProvider
 } from "./compose/generate-audio.js";
 
 export { removeBackgroundWithModel } from "./compose/remove-background.js";
 export type {
   RemoveBackgroundWithModelOptions,
-  RemoveBackgroundWithModelUnified,
+  RemoveBackgroundWithModelUnified
 } from "./compose/remove-background.js";
 
 export {
+  addSubtitles,
+  lipSync,
   merge,
   reframe,
-  lipSync,
-  addSubtitles,
   removeBackground,
-  replaceGreenScreen,
   removeImageBackground,
+  replaceGreenScreen
 } from "./compose/operations.js";
 export type {
+  LipSyncOptions,
   MergeOptions,
   ReframeOptions,
-  LipSyncOptions,
-  SubtitlesOptions,
   RemoveBackgroundOptions,
-  ReplaceGreenScreenOptions,
   RemoveImageBackgroundOptions,
+  ReplaceGreenScreenOptions,
+  SubtitlesOptions
 } from "./compose/operations.js";
 
 export { video } from "./compose/video.js";
 export type { CreateVideoOptions } from "./compose/video.js";
 
 export type {
-  Video,
-  Image,
   Audio,
-  VideoNode,
-  VideoOperation,
-  ImageOperation,
   AudioOperation,
   ExecutionPlan,
+  Image,
+  ImageOperation,
   JobNode,
   OperationType,
+  Video,
+  VideoNode,
+  VideoOperation
 } from "./core/video.js";
+
+export { getSynthomeApiKey, tryGetSynthomeApiKey } from "./utils/api-key.js";
