ee637bac3030dcc42b775f7bd7355ea95ae81574
Dmitry Dubovetzky <dubovetzky@me.com>
2025-11-24 13:10:55 +0100

    Cleanup

--- packages/video-render/src/index.ts
+++
@@ -1,9 +0,0 @@
-export * from "./captions/captions";
-export * from "./render";
-export * from "./resource-cache";
-export * from "./seed";
-export * from "./utils";
-export * from "./utils/format-dimensions";
-export * from "./utils/watermark-position";
-export { VideoRenderService } from "./video-render-service";
-
